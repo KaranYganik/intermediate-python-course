5ffd7f7f2d689484486232c7975d0d0eefe1e2d9
Karan B Yagnik <71938601+KaranYganik@users.noreply.github.com>
2020-09-26 17:50:48 +0000

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -2,9 +2,7 @@ import random
 def main(min,max):
 	while True:
 		print ("Rolling the dices...")
-		print ("The values are....")
-		print (random.randint(min, max))
-			
+		print(f"Your number is {random.randint(min, max)}")	
 		roll_again = input("Roll the dices again?")
 		roll_againn = roll_again.lower()
 		if roll_againn == 'no' or roll_againn =='n':
