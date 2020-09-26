7a2480ce559cdd1f89c2eca0ec7fda12b47e2b8a
Karan B Yagnik <71938601+KaranYganik@users.noreply.github.com>
2020-09-26 17:54:45 +0000

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,3 +1,4 @@
+'''
 import random
 def main(min,max):
 	while True:
@@ -23,3 +24,15 @@ def roll(min,max):
 
 if __name__== "__main__":
   roll(1,6)
+'''
+import random
+
+def roll(min, max):
+    while True: 
+        print("Rolling the dices...")
+        print(f"Your number is {random.randint(min, max)}")
+        answer = input("Do you want to roll the dices again? (y/n) ")
+        if answer.lower() == "n":
+            break
+
+roll(1, 6)
